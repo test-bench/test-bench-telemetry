222371e0d6edcd033ff51bcf53ada09ac0f0e68e
Nathan Ladd <nathanladd@gmail.com>
2022-10-18 18:08:27 -0500

    Controls: Line Number

--- lib/test_bench/telemetry/controls.rb
+++ lib/test_bench/telemetry/controls.rb
@@ -2,3 +2,4 @@ require 'test_bench/telemetry/controls/random'
 require 'test_bench/telemetry/controls/time'
 
 require 'test_bench/telemetry/controls/comment'
+require 'test_bench/telemetry/controls/line_number'
