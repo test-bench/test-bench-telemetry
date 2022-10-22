38603c46cfd8377bf0db6ed3c0a0e609ac5e8e5b
Nathan Ladd <nathanladd@gmail.com>
2022-10-22 14:24:11 -0500

    File Sink: Open

---
+++ test/automated/file_sink/open.rb
@@ -0,0 +1,26 @@
+require_relative '../automated_init'
+
+context "File Sink" do
+  context "Open" do
+    path = Controls::File::Random.example(basename: 'file-sink-open-test')
+
+    open_file = false
+    path_corresponds = false
+
+    file_sink = Telemetry::Sink::File.open(path) do |file_sink, file|
+      open_file = file_sink.io == file && file.is_a?(File)
+      path_corresponds = file.path == path
+    end
+
+    test "Opens file at path" do
+      assert(open_file && path_corresponds)
+    end
+
+    test "Closes the file" do
+      assert(file_sink.io.closed?)
+    end
+
+  ensure
+    File.unlink(path) if File.exist?(path)
+  end
+end
