ade0a84249694c4a3f2518184cbedf3071a02162
Nathan Ladd <nathanladd@gmail.com>
2022-10-21 21:38:07 -0500

    Handler Sink: Class Macro

--- lib/test_bench/telemetry/controls/handler.rb
+++ lib/test_bench/telemetry/controls/handler.rb
@@ -2,6 +2,28 @@ module TestBench
   class Telemetry
     module Controls
       module Handler
+        def self.example
+          Example.new
+        end
+
+        class Example
+          include Telemetry::Sink::Handler
+
+          attr_accessor :handled_event
+
+          handle Event::SomeEvent do |some_event|
+            self.handled_event = some_event
+          end
+
+          def handled?(event=nil)
+            return false if handled_event.nil?
+
+            return true if event.nil?
+
+            event == handled_event
+          end
+        end
+
         module Method
           def self.example(event_type=nil)
             event_type ||= Event::SomeEvent.event_type
