417372abe9ff3f3d7472f37a8ee701cf6be76a62
Nathan Ladd <nathanladd@gmail.com>
2022-10-22 12:41:49 -0500

    Handler Sink: Handler Method

---
+++ lib/test_bench/telemetry/sink/handler.rb
@@ -0,0 +1,57 @@
+module TestBench
+  class Telemetry
+    module Sink
+      module Handler
+        def self.included(cls)
+          cls.class_exec do
+            include Sink
+
+            extend HandlerMethod
+          end
+        end
+
+        def handler_method(event_or_event_data)
+          handler_method = self.class.handler_method(event_or_event_data)
+
+          if respond_to?(handler_method)
+            handler_method
+          else
+            nil
+          end
+        end
+
+        module HandlerMethod
+          def handler_method(event_or_event_data)
+            if event_or_event_data.is_a?(Event)
+              event = event_or_event_data
+              event_type = event.event_type
+            else
+              event_data = event_or_event_data
+              event_type = event_data.type
+            end
+
+            HandlerMethod.get(event_type)
+          end
+
+          def self.get(event_type)
+            handler_methods[event_type]
+          end
+
+          def self.handler_methods
+            @handler_methods ||= Hash.new do |handler_methods, event_type|
+              handler_method = get!(event_type)
+
+              handler_methods[event_type] = handler_method
+            end
+          end
+
+          def self.get!(event_type)
+            event_name = Event::EventName.get(event_type)
+
+            :"handle_#{event_name}"
+          end
+        end
+      end
+    end
+  end
+end
