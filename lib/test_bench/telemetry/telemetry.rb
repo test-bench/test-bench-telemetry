0c48eac98ae4706c99e4650e1a9c666be6bbe1b0
Nathan Ladd <nathanladd@gmail.com>
2022-10-22 15:24:11 -0500

    Telemetry: Register Sink

--- lib/test_bench/telemetry/telemetry.rb
+++ lib/test_bench/telemetry/telemetry.rb
@@ -1,5 +1,7 @@
 module TestBench
   class Telemetry
+    RegistrationError = Class.new(RuntimeError)
+
     def sinks
       @sinks ||= []
     end
@@ -34,6 +36,18 @@ module TestBench
 
     def self.process_id
       ::Process.pid
+    end
+
+    def register(sink)
+      if registered?(sink)
+        raise RegistrationError, "Already registered #{sink.inspect}"
+      end
+
+      sinks.unshift(sink)
+    end
+
+    def registered?(sink)
+      sinks.include?(sink)
     end
   end
 end
