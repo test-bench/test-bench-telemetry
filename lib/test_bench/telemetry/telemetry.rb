dc928ddcaa9c71f227b0e4ba44df391e43d3e062
Nathan Ladd <nathanladd@gmail.com>
2022-10-23 19:09:02 -0500

    Telemetry: Event Type Variants

--- lib/test_bench/telemetry/telemetry.rb
+++ lib/test_bench/telemetry/telemetry.rb
@@ -1,5 +1,7 @@
 module TestBench
   class Telemetry
+    include Event::Events
+
     RegistrationError = Class.new(RuntimeError)
 
     def sinks
@@ -26,6 +28,20 @@ module TestBench
 
     def registered?(sink)
       sinks.include?(sink)
+    end
+
+    Event.each_event_type do |event_type|
+      event_class = Event.const_get(event_type)
+
+      event_type_method_cased = Event::Type.method_cased(event_type)
+
+      module_eval(<<~RUBY, __FILE__, __LINE__)
+      def record_#{event_type_method_cased}(*values)
+        event = #{event_class}.new(*values)
+
+        record(event)
+      end
+      RUBY
     end
 
     def current_time(now)
