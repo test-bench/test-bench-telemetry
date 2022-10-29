7e13e32a52899c6aa8486aff2db779a05ba218f8
Nathan Ladd <nathanladd@gmail.com>
2022-10-29 17:21:52 -0500

    Capture (Sink): Handle Event

---
+++ lib/test_bench/telemetry/sink/capture.rb
@@ -0,0 +1,26 @@
+module TestBench
+  class Telemetry
+    module Sink
+      class Capture
+        include Sink
+
+        def raw_records
+          @raw_records ||= []
+        end
+
+        def generate_record
+          @generate_record ||= Record::Generate.new
+        end
+        attr_writer :generate_record
+
+        def call(event)
+          record = generate_record.(event)
+
+          raw_records.push(record)
+
+          record
+        end
+      end
+    end
+  end
+end
