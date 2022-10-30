fc021200c0814a1a8924b50a31831baacf2b88b5
Nathan Ladd <nathanladd@gmail.com>
2022-10-30 11:40:21 -0500

    Substitute (Telemetry): One Record Methods

--- lib/test_bench/telemetry/substitute.rb
+++ lib/test_bench/telemetry/substitute.rb
@@ -20,6 +20,16 @@ module TestBench
           event_type_method_cased = Event::Type.method_cased(event_type)
 
           module_eval(<<~RUBY, __FILE__, __LINE__)
+          def one_#{event_type_method_cased}_record(*arguments, **keyword_arguments, &block)
+            block ||= proc { true }
+
+            one_record(*arguments, **keyword_arguments) do |compare_event_type, *event_values|
+              if compare_event_type == #{event_type.inspect}
+                block.(*event_values)
+              end
+            end
+          end
+
           def any_#{event_type_method_cased}_record?(*arguments, **keyword_arguments, &block)
             block ||= proc { true }
 
@@ -43,6 +53,7 @@ module TestBench
           RUBY
         end
 
+        def one_record(...) = capture_sink.one_record(...)
         def any_record?(...) = capture_sink.any_record?(...)
         def records(...) = capture_sink.records(...)
       end
---
+++ test/automated/substitute/one_record.rb
@@ -0,0 +1,54 @@
+require_relative '../automated_init'
+
+context "Substitute" do
+  context "One Record Methods" do
+    other_type_event = Controls::Event.example
+
+    events = Controls::Events.examples
+    other_events = Controls::Events.examples(random: true)
+
+    events.zip(other_events).each do |(event, other_event)|
+      event_type = event.event_type
+
+      event_type_method_cased = Telemetry::Event::Type.method_cased(event_type)
+
+      one_record_method = :"one_#{event_type_method_cased}_record"
+
+      context "#{one_record_method}" do
+        substitute = Telemetry::Substitute.build
+
+        substitute.record(event)
+        substitute.record(other_event)
+        substitute.record(other_type_event)
+
+        context "One Record Matches" do
+          record = substitute.public_send(one_record_method) do |*values|
+            values != other_event.values
+          end
+
+          test "Matches the record" do
+            assert(record.event == event)
+          end
+        end
+
+        context "More Than One Record Matches" do
+          test "Is an error" do
+            assert_raises(Telemetry::Sink::Capture::MatchError) do
+              substitute.public_send(one_record_method)
+            end
+          end
+        end
+
+        context "No Records Match" do
+          any_record = substitute.public_send(one_record_method) { false }
+
+          comment any_record.inspect
+
+          test do
+            refute(any_record)
+          end
+        end
+      end
+    end
+  end
+end
