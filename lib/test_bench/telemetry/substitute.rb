7d90ac0a05b1e0dfba8495290c3ad1820927b5ac
Nathan Ladd <nathanladd@gmail.com>
2022-10-30 11:37:36 -0500

    Substitute (Telemetry): Record Event

---
+++ lib/test_bench/telemetry/substitute.rb
@@ -0,0 +1,25 @@
+module TestBench
+  class Telemetry
+    module Substitute
+      def self.build
+        Telemetry.build
+      end
+
+      class Telemetry < Telemetry
+        attr_accessor :process_id
+        attr_accessor :current_time
+
+        def sink
+          @sink ||= Substitute::Sink.new
+        end
+        attr_writer :sink
+
+        def self.build
+          instance = new
+          instance.register(instance.sink)
+          instance
+        end
+      end
+    end
+  end
+end
