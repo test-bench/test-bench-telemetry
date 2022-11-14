362ce5f62e26324eccec90cf4f24b4c9afeee05b
Nathan Ladd <nathanladd@gmail.com>
2022-11-14 10:18:51 -0600

    Telemetry: Record

---
+++ lib/test_bench/telemetry/telemetry.rb
@@ -0,0 +1,43 @@
+module TestBench
+  class Telemetry
+    def sinks
+      @sinks ||= []
+    end
+    attr_writer :sinks
+
+    def record(event)
+      if event.is_a?(Event)
+        event.metadata ||= Event::Metadata.new
+
+        event_data = Event::Export.(event)
+      else
+        event_data = event
+      end
+
+      event_data.process_id ||= process_id
+      event_data.time ||= current_time
+
+      sinks.each do |sink|
+        sink.receive(event_data)
+      end
+
+      event_data
+    end
+
+    def current_time
+      self.class.current_time
+    end
+
+    def process_id
+      self.class.process_id
+    end
+
+    def self.current_time
+      ::Time.now
+    end
+
+    def self.process_id
+      ::Process.pid
+    end
+  end
+end
