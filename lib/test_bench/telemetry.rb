b0a4bff9c70bc7eb9b3037a0e5f40b2194f235f4
Nathan Ladd <nathanladd@gmail.com>
2022-11-14 10:18:51 -0600

    Telemetry: Record

--- lib/test_bench/telemetry.rb
+++ lib/test_bench/telemetry.rb
@@ -9,3 +9,5 @@ require 'test_bench/telemetry/sink'
 require 'test_bench/telemetry/sink/file'
 require 'test_bench/telemetry/sink/handler/event_registry'
 require 'test_bench/telemetry/sink/handler'
+
+require 'test_bench/telemetry/telemetry'
