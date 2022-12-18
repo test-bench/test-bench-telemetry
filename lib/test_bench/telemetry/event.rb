ac1605da373cc5417821df50011194c6e253de41
Nathan Ladd <nathanladd@gmail.com>
2022-12-18 16:08:33 -0600

    Event: Event Name

--- lib/test_bench/telemetry/event.rb
+++ lib/test_bench/telemetry/event.rb
@@ -1,20 +1,29 @@
 module TestBench
   class Telemetry
     module Event
-      def self.define
+      def self.define(&blk)
         Struct.new(:metadata) do
           include Event
+
+          if not blk.nil?
+            instance_exec(&blk)
+          end
         end
       end
 
       def self.included(cls)
         cls.class_exec do
           extend EventType
+          extend EventName
         end
       end
 
       def event_type
         self.class.event_type
+      end
+
+      def event_name
+        self.class.event_name
       end
 
       module EventType
@@ -26,6 +35,26 @@ module TestBench
           *, inner_namespace = self.name.split('::')
 
           inner_namespace.to_sym
+        end
+      end
+
+      module EventName
+        def event_name
+          EventName.get(event_type)
+        end
+
+        def self.get(event_type)
+          pascal_cased = event_type.to_s
+
+          underscore_cased = pascal_cased.gsub(%r{(?:\A|[a-z])[A-Z]+}) do |match_text|
+            if ('a'..'z').include?(match_text[0])
+              match_text.insert(1, '_')
+            end
+            match_text.downcase!
+            match_text
+          end
+
+          underscore_cased.to_sym
         end
       end
 
