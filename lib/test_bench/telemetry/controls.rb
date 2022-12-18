60a799dc161875fe6d767ce684cba79f6b24740e
Nathan Ladd <nathanladd@gmail.com>
2022-12-18 16:00:46 -0600

    Event: Event Type

--- lib/test_bench/telemetry/controls.rb
+++ lib/test_bench/telemetry/controls.rb
@@ -4,3 +4,6 @@ require 'test_bench/telemetry/controls/process_id'
 require 'test_bench/telemetry/controls/time'
 
 require 'test_bench/telemetry/controls/event_data'
+
+require 'test_bench/telemetry/controls/event'
+require 'test_bench/telemetry/controls/event/metadata'
