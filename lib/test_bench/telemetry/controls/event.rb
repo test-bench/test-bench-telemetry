3dca0764a6daff8e45d6178ee22ea368c163e3d5
Nathan Ladd <nathanladd@gmail.com>
2022-12-18 16:21:25 -0600

    Event: Export

--- lib/test_bench/telemetry/controls/event.rb
+++ lib/test_bench/telemetry/controls/event.rb
@@ -2,19 +2,55 @@ module TestBench
   class Telemetry
     module Controls
       module Event
-        def self.example(metadata: nil, process_id: nil, time: nil, event_class: nil)
+        def self.example(some_attribute: nil, some_other_attribute: nil, metadata: nil, process_id: nil, time: nil, event_class: nil)
+          some_attribute ||= self.some_attribute
+          some_other_attribute ||= self.some_other_attribute
           metadata ||= Metadata.example(process_id:, time:)
           event_class ||= SomeEvent
 
           event = event_class.new
+          event.some_attribute = some_attribute
+          event.some_other_attribute = some_other_attribute
           event.metadata = metadata
           event
         end
 
+        def self.other_example
+          metadata = Metadata.other_example
+
+          Event.example(event_class: SomeOtherEvent, metadata:)
+        end
+
+        def self.random
+          some_attribute, some_other_attribute = Data.random
+          metadata = Metadata.random
+
+          Event.example(some_attribute:, some_other_attribute:, metadata:)
+        end
+
+        def self.some_attribute = Data.some_attribute
+        def self.some_other_attribute = Data.some_other_attribute
         def self.process_id = Controls::EventData.process_id
         def self.time = Controls::EventData.time
 
-        SomeEvent = TestBench::Telemetry::Event.define
+        SomeEvent = TestBench::Telemetry::Event.define(:some_attribute, :some_other_attribute)
+        SomeOtherEvent = TestBench::Telemetry::Event.define(:some_attribute, :some_other_attribute)
+
+        module Data
+          def self.example
+            [some_attribute, some_other_attribute]
+          end
+
+          def self.random
+            [
+              "#{some_attribute}-#{Random.string}",
+              "#{some_other_attribute}-#{Random.string}"
+            ]
+          end
+
+          def self.some_attribute = 'some-value'
+          def self.some_other_attribute = 'some-other-value'
+        end
 
         module Metadata
           def self.example(process_id: nil, time: nil)
