ef3a40a1406ca62f359ca2209c342ba876997931
Nathan Ladd <nathanladd@gmail.com>
2023-07-17 09:46:46 -0500

    Projection Sink: Class Macro

--- lib/test_bench/telemetry/controls/projection.rb
+++ lib/test_bench/telemetry/controls/projection.rb
@@ -12,6 +12,14 @@ module TestBench
           include Telemetry::Sink::Projection
 
           receiver_name :some_receiver
+
+          apply Event::SomeEvent do |some_event|
+            receiver.event(some_event)
+          end
+
+          def applied?(event=nil)
+            receiver.event?(event)
+          end
         end
 
         module ApplyMethod
