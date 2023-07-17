76ec8e55c1c4c6d9721e6baa4afd616b914aa563
Nathan Ladd <nathanladd@gmail.com>
2023-07-17 10:01:14 -0500

    Projection Sink: Apply

--- lib/test_bench/telemetry/controls/projection.rb
+++ lib/test_bench/telemetry/controls/projection.rb
@@ -11,14 +11,55 @@ module TestBench
         class Example
           include Telemetry::Sink::Projection
 
+          attr_accessor :applied_event_data
+
           receiver_name :some_receiver
 
           apply Event::SomeEvent do |some_event|
             receiver.event(some_event)
           end
 
+          def apply_event_data(event_data)
+            self.applied_event_data = event_data
+          end
+
           def applied?(event=nil)
             receiver.event?(event)
+          end
+
+          def applied_event_data?(event_data)
+            self.applied_event_data == event_data
+          end
+        end
+
+        module NoApplyMethod
+          def self.example(receiver=nil)
+            receiver ||= Receiver.example
+
+            Example.new(receiver)
+          end
+
+          class Example
+            include Telemetry::Sink::Projection
+          end
+        end
+
+        module NoArgument
+          def self.example(receiver=nil)
+            receiver ||= Receiver.example
+
+            Example.new(receiver)
+          end
+
+          class Example
+            include Telemetry::Sink::Projection
+
+            attr_accessor :applied_event
+            def applied? = !!applied_event
+
+            apply Event::SomeEvent do
+              self.applied_event = true
+            end
           end
         end
 
