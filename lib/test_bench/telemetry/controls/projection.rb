13e50f3f2feef7128cc4f2d27978559c420c6574
Nathan Ladd <nathanladd@gmail.com>
2023-07-17 09:24:44 -0500

    Projection Sink: Apply Method

--- lib/test_bench/telemetry/controls/projection.rb
+++ lib/test_bench/telemetry/controls/projection.rb
@@ -13,6 +13,22 @@ module TestBench
 
           receiver_name :some_receiver
         end
+
+        module ApplyMethod
+          def self.example(event_type=nil)
+            event_type ||= Event::SomeEvent.event_type
+
+            event_name = TestBench::Telemetry::Event::EventName.get(event_type)
+
+            :"apply_#{event_name}"
+          end
+
+          def self.other_example
+            event_type = Event::SomeOtherEvent.event_type
+
+            example(event_type)
+          end
+        end
       end
     end
   end
